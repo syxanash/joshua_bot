9c26f007996eb50df7fa2ca95ff74a17473969cf
syxanash <syxanash@gmail.com>
2015-11-14 14:34:40 +0100

    artist plugin fixed

--- lib/plugins/artist.rb
+++ lib/plugins/artist.rb
@@ -8,28 +8,32 @@ class Artist < Plugin
   end
 
   def show_usage
-    bot.api.sendMessage(chat_id: message.chat.id, text: "if you want get info about a artist type:\n/artist *Maher Zain*")
+    bot.api.sendMessage(chat_id: message.chat.id, text: "if you want get info about an artist type:\n/artist *Maher Zain*")
   end
 
   def do_stuff(match_results)
     artist_name = URI::encode(match_results[1])
-   
+
     json_resp = RestClient.get("http://api.spotify.com/v1/search?q=#{artist_name}&type=artist")
     decoded = JSON.parse(json_resp)
 
-    if decoded
-
-      name = "#{decoded["artists"]["items"][0]["name"]}"
-      category = "#{decoded["artists"]["items"][0]["type"]}"
-      followers = "#{decoded["artists"]["items"][0]["followers"]["total"]}"
-      popularity = "#{decoded["artists"]["items"][0]["popularity"]}"
-
-      bot.api.sendMessage(chat_id: message.chat.id, text: "I found on Spotify:\n Name : #{name} \n Category : #{category}\n  Popularity : #{popularity}\n Total Follower : #{followers}\n ")
+    if decoded['artists']['items'].empty?
+      bot.api.sendMessage(chat_id: message.chat.id, text: 'Your artist name doesn\'t exist on my database 😞')
     else
-      bot.api.sendMessage(chat_id: message.chat.id, text: "Your artist name doesn't exist on my database 😞")
+      name = "#{decoded['artists']['items'][0]['name']}"
+      category = "#{decoded['artists']['items'][0]['type']}"
+      followers = "#{decoded['artists']['items'][0]['followers']['total']}"
+      popularity = "#{decoded['artists']['items'][0]['popularity']}"
+
+      output_message = <<-MSG
+🎶 I found on Spotify:
+Name: #{name}
+Category: #{category}
+Popularity: #{popularity}
+Total Follower: #{followers}
+MSG
+
+      bot.api.sendMessage(chat_id: message.chat.id, text: output_message)
     end
   end
 end
-
-
-
