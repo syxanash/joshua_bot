2ffbd6c8e0561a44276cf22575e8e59fbe241a90
syxanash <syxanash@gmail.com>
2015-12-01 12:01:57 +0100

    add new plugin to show plugins list

---
+++ lib/plugins/help.rb
@@ -0,0 +1,23 @@
+class Help < Plugin
+  def command
+    /\/help$/
+  end
+
+  def show_usage
+    bot.api.sendMessage(chat_id: message.chat.id, text: 'type /help to see commands list')
+  end
+
+  def do_stuff(match_results)
+
+    text_to_show = "You can give me the following commands:\n\n"
+    plugin_classes = Plugin.descendants
+
+    plugin_classes.each do |plugin_name|
+      text_to_show += "/#{plugin_name.to_s.downcase}\n"
+    end
+
+    text_to_show += "\nc'mon I'm all ears! 🔊👂"
+
+    bot.api.sendMessage(chat_id: message.chat.id, text: text_to_show)
+  end
+end
