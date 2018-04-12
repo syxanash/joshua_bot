f7e25c022db8ea1a6050752cd27ae8c4e05fbd30
Simone Marzulli <syxanash@users.noreply.github.com>
2018-04-12 22:08:13 +0100

    Update morra.rb

--- lib/plugins/morra.rb
+++ lib/plugins/morra.rb
@@ -25,11 +25,22 @@ class Morra < Plugin
     MUST_REPLY
   end
 
-  def do_answer(answer)
-    choosen = @values[Random.rand(@values.size)]
+  def do_answer(human_choice)
+    bot_choice = @values[Random.rand(@values.size)]
+
+    win_rules = [['📄', '🗿'], ['🗿', '✂️'], ['✂️', '📄']]
+    winner_message = 'no winners 😞'
+
+    win_rules.each do |rule|
+      if rule[0] == bot_choice && rule[1] == human_choice
+        winner_message = 'the machine #masterrace wins! 🤖'
+      elsif rule[1] == bot_choice && rule[0] == human_choice
+        winner_message = 'filthy human beings...'
+      end
+    end
 
     kb = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
-    bot.api.send_message(chat_id: message.chat.id, text: "I choose #{choosen}", reply_markup: kb)
+    bot.api.send_message(chat_id: message.chat.id, text: "I choose #{bot_choice}, #{winner_message}", reply_markup: kb)
 
     # if do answer doesn't need new inputs return STOP_REPLYING constant
     # to make sure the next message sent by user won't be passed to do_answer
